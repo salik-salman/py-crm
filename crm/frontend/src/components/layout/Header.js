a2a3a573f5f63d9945e939918e88c0075cec33cc
salik-salman <salik.salman13@gmail.com>
2021-11-23 04:30:21 +0500

    update

--- crm/frontend/src/components/layout/Header.js
+++ crm/frontend/src/components/layout/Header.js
@@ -118,7 +118,7 @@ export default class Header extends Component {
         
 <nav className="main-header navbar navbar-expand navbar-white navbar-light">
   {/* Left navbar links */}
-  <ul className="navbar-nav">
+  {/* <ul className="navbar-nav">
     <li className="nav-item">
       <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars" /></a>
     </li>
@@ -128,7 +128,7 @@ export default class Header extends Component {
     <li className="nav-item d-none d-sm-inline-block">
       <a href="#" className="nav-link">Contact</a>
     </li>
-  </ul>
+  </ul> */}
   {/* Right navbar links */}
   <ul className="navbar-nav ml-auto">
     {/* Navbar Search */}
