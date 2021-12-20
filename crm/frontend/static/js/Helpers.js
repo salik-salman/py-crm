d3823cb40a57c792808c8a55dfa75332939cb4da
salik-salman <salik.salman13@gmail.com>
2021-12-21 02:21:35 +0500

    Update Helpers.js

--- crm/frontend/static/js/Helpers.js
+++ crm/frontend/static/js/Helpers.js
@@ -1,10 +1,14 @@
+import axios from "axios";
+import React from "react";
+import { GET_MENU } from "../../src/actions/types";
+var isAuthenticated_user;
 const helpers = {
     default: function(){
         $(document).ready(function(){
             var h = helpers;
             h.calculateLine();
             h.Login_check();
-            h.Api_check();
+            h.queryAll();
         })
     },
     calculateLine: function(){
@@ -30,12 +34,35 @@ const helpers = {
           }
         toastr.error(label,message)  
     },
-    Login_check: function(){
-        console.log('Login_check')
+    isAuthenticated : function(check=''){
+        check == true ? isAuthenticated_user = true : isAuthenticated_user = false;
+if(isAuthenticated_user == true){
+    return true;
+}else{
+    return false;
+}
     },
-    Api_check: function(){
-        console.log('Api_Check')
-    }
+    Login_check: function(email,pass){
+        if(email == 'sa@sa.sa' && pass == 'sa'){
+        this.isAuthenticated(true)
+        // return isAuthenticated_user;
+        console.log(isAuthenticated_user)
+        return true;
+    }else{
+        this.isAuthenticated(false)
+        return false;
+        }
+    },
+    queryAll: function(query = '',table) {
+// export const getMenu = () => dispatch =>{
+axios.get('/api/'+table+'/')
+    .then(res => {
+    //   dispatch(
+          console.log(res.data)
+    //   )  
+    }).catch(err => console.log(err.response));
+}
+    // }
 }
 
 export default helpers;
